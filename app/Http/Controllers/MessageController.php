3effdb8615649668465cef23488ebeb26e855898
Dianahamammeh.com <daianahamammeh@gmail.com>
2024-05-02 14:48:22 +0300

    my create a curd of message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Log;
+
 
 class MessageController extends Controller
 {
@@ -12,7 +14,9 @@ class MessageController extends Controller
      */
     public function index()
     {
-        //
+        $messages = Message::all();
+       
+       
     }
 
     /**
@@ -20,7 +24,19 @@ class MessageController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
+        try {
+            $messages = Messages::create([
+                'title'     => $request->title,
+                'body'      => $request->body,
+            ]);
+            return response()->json(new MessagesResource($messages),"status",200);
+        } catch (\Throwable $th) {
+            Log::error($th);
+            return response()->json("Error", 201);
+        }
+
+        
     }
 
     /**
@@ -28,15 +44,32 @@ class MessageController extends Controller
      */
     public function show(Message $message)
     {
-        //
+        try {
+            return  response()->json(new MessagesResource($message),"status",200);
+        } catch (\Throwable $th) {
+            Log::error($th);
+            return  response()->json(null,"user not found",404);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Message $message)
+    public function update(UpdateMessageRequest $request, Message $message)
     {
-        //
+        try {
+                if ($request->has('title')) {
+                    $message->title = $request->title;
+                }
+                if ($request->has('body')) {
+                    $message->body = $request->body;
+                }
+                $message->save();
+            return $this->customeResponse(new MessageResource($message),"Message updated successfully",200);
+        } catch (\Throwable $th) {
+            Log::error($th);
+            return $this->customeResponse(null,"Error!!,there is something not correct",500);
+        }
     }
 
     /**
@@ -44,6 +77,13 @@ class MessageController extends Controller
      */
     public function destroy(Message $message)
     {
-        //
+       
+        try {
+            $message->delete();
+            return $this->customeResponse("","Message deleted",200);
+        } catch (\Throwable $th) {
+            Log::error($th);
+            return $this->customeResponse(null,"Error!!,there is something not correct",500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\MessageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::apiResource('messages',MessageController::class);
 
 Route::apiResource('projects',ProjectController::class);

0577ae6c06bcaa0398830902fc8c1d3ddc79346b
firassemgey <firas.semgey@gmail.com>
2024-05-03 20:40:52 +0300

    Firas first

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,15 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\adminLogin;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function login() {
-
+    public function login(adminLogin $request)
+    {
+        $email='admin@admin.com';
+        $password='admin@123';
+        if($request->email == $email && $request->password == $password){
+            return response()->json([
+                'status' => 'success',
+                'dashBoard' =>true,
+            ]);
+        }else{
+            return response()->json([
+                'status' => 'error',
+                'dashBoard' => false,
+            ], 401);
+        }
     }
-
-    public function logout() {
-
+    public function logout(Request $request) {
+        if(isset($request->logout)){
+            return response()->json([
+                'status' => 'success',
+                'dashBoard' =>false,
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AdminController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('logout', 'logout');
 });
-
-
-Route::apiResource('projects',ProjectController::class);
